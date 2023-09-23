643ba34df71df5b99cb6c4cf8f51f00b61fc32c7
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-09-22 20:30:44 -0400

    Init Project of Random Design Components
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Menu } from 'lucide-react';
+
+import MagneticComponent from './components/MagneticComponent';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <div className='flex flex-col w-full h-screen items-center justify-center'>
+        <MagneticComponent>
+          <Button>
+            <Menu />
+          </Button>
+        </MagneticComponent>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MagneticComponent.tsx
@@ -0,0 +1,47 @@
+/* Magnetic Component Created with Framer Motion */
+
+import { useRef, useState, PropsWithChildren, MouseEvent } from 'react';
+import { motion } from 'framer-motion';
+
+interface MagneticComponentProps extends PropsWithChildren {
+  className?: string;
+  modifier?: { x: number; y: number };
+}
+
+const MagneticComponent = ({
+  children,
+  className,
+  modifier = { x: 1, y: 1 },
+}: MagneticComponentProps) => {
+  const ref = useRef<HTMLDivElement>(null!);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const mouseMove = (e: MouseEvent) => {
+    const { clientX, clientY } = e;
+    const { width, height, left, top } = ref.current.getBoundingClientRect();
+    const x = (clientX - (left + width / 2)) * modifier.x;
+    const y = (clientY - (top + height / 2)) * modifier.y;
+    setPosition({ x, y });
+  };
+
+  const mouseLeave = () => {
+    setPosition({ x: 0, y: 0 });
+  };
+
+  const { x, y } = position;
+
+  return (
+    <motion.div
+      className={className}
+      ref={ref}
+      onMouseMove={mouseMove}
+      onMouseLeave={mouseLeave}
+      animate={{ x, y }}
+      transition={{ type: 'spring', stiffness: 150, damping: 8, mass: 1 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default MagneticComponent;
